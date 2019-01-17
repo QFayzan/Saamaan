f6d9581493fd2313c98bad07e248743f608ddb70
QFayzan <qfayzan@gmail.com>
2019-01-17 15:33:31 +0500

    added the Becoming driver instruction page
    Instruction page on becoming driver

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    public function promote()
+    {
+    
+    }
+    public function editUser()
+    {
+    
+    }
+    
+    public function editDetail()
+    {
+    
+    }
+    public function edit()
+    {
+    
+    }
+    
+}
--- resources/views/Drivers/create.blade.php
+++ resources/views/Drivers/create.blade.php
@@ -8,8 +8,27 @@
                     <div class="card">
                         <div class="card-header">Request to be a Driver</div>
                         <div class="card-body">
-                        <form>
-                        </form>
+                        <p>
+                            Being a driver on Saaman is a great freelancing opportunity.
+                            In order to be a Driver for Saamaan and take deliveries on our behalf there are some
+                            basic requirements that any potential driver must fulfil.
+                            <ul>
+                               <li> You must have a verified CNIC and a Driver's licence (Learner's license is invalid).</li>
+                                <li>A vehicle on your own name is a standard requirement.</li>
+                                <li>Vehicle Ownsership documents WILL be verified by us(which may take 2 business days).</li>
+                                <li>Client's personal legal history would be taken in account during the process.</li>
+                                <li>The company holds full control over a driver's membership which may be terminated anytime.
+                            </ul>
+                            <p>
+                                In order to be a driver any candidate must approach our one of may offices in their respective cities city.
+                                This step is Mandatory.
+                            </p>
+                            <p> Our Offices include
+                            <ul>
+                                <li>XYZ Colony, Lahore</li>
+                                <li>ABC Market, Islamabad</li>
+                                <li>JKL Road, Karachi</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
