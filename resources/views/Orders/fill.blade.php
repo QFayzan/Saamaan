e26c518faa560cd761886094772bc6cd6f497fd5
QFayzan <qfayzan@gmail.com>
2019-01-17 15:54:26 +0500

    form was changed but change not working:(

--- resources/views/Orders/fill.blade.php
+++ resources/views/Orders/fill.blade.php
@@ -4,29 +4,30 @@
     <form method="POST" action="{{ route('details.store',$order->id) }}">
         @csrf
      <div class="form-group">
-         <label for="name">Name</label>
+         <label for="name">Brief explanation of order</label>
          <input id="name" type="text" name="name" class="form-control {{ $errors->has('name') ? "is-invalid" : "" }}" value="{{ old('name') }}" placeholder="Name">
          @if($errors->has('name'))
              <strong class="invalid-feedback">{{ $errors->first('name') }}</strong>
          @endif
      </div>
         <div class="form-group">
-            <label for="quantity">Quantity</label>
+            <label for="quantity">Quantity or number of items in the order</label>
             <input id="quantity" type="text" name="Quantity" class="form-control {{ $errors->has('Quantity') ? "is-invalid" : "" }}" value="{{ old('Quantity') }}" placeholder="Quantity">
             @if($errors->has('Quantity'))
                 <strong class="invalid-feedback">{{ $errors->first('Quantity') }}</strong>
             @endif
         </div>
         <div class="form-group">
-            <label for="weight">Weight</label>
+            <label for="weight">Weight of the order this must be in kilograms</label>
             <input id="weight" type="text" name="Weight" class="form-control {{ $errors->has('Weight') ? "is-invalid" : "" }}" value="{{ old('Weight') }}" placeholder="Weight">
-            @if($errors->has('Weight'))
+            @if($errors->has('Weight in KG'))
                 <strong class="invalid-feedback">{{ $errors->first('Weight') }}</strong>
             @endif
         </div>
         <div class="form-group">
-            <label for="dimension">Dimension</label>
-            <input id="dimension" type="text" name="Dimension" class="form-control {{ $errors->has('Dimension') ? "is-invalid" : "" }}" value="{{ old('Dimension') }}" placeholder="Dimension">
+            <label for="dimension">Dimension in meters only </label>
+            <input id="dimension" type="text" name="Dimension" class="form-control {{ $errors->has('Dimension') ? "is-invalid"
+            : "" }}" value="{{ old('Dimension') }}" placeholder="eg 1.25 1.10 means 1.25 meters by 1.10 meters">
             @if($errors->has('Dimension'))
                 <strong class="invalid-feedback">{{ $errors->first('Dimension') }}</strong>
             @endif
