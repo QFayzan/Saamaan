f6906f85ac5961aac1ae9347a17748c37e6b8b09
waqar1361 <36884993+waqar1361@users.noreply.github.com>
2019-03-17 23:13:25 +0500

    Organizations

---
+++ resources/views/organizations/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Edit Organization</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('organizations.update',$organization->id)}}">
+                            @csrf
+                            @method('patch')
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input id="name" type="text" name="name"
+                                       class="form-control {{ $errors->has('name') ? "is-invalid" : "" }}"
+                                       value="{{ $organization->name }}" placeholder="Name">
+                                @if($errors->has('name'))
+                                    <strong class="invalid-feedback">{{ $errors->first('name') }}</strong>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label for="address">Address</label>
+                                <input id="address" type="text" name="address"
+                                       class="form-control {{ $errors->has('address') ? "is-invalid" : "" }}"
+                                       value="{{ $organization->address }}" placeholder="Address">
+                                @if($errors->has('address'))
+                                    <strong class="invalid-feedback">{{ $errors->first('address') }}</strong>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label for="contact">Contact</label>
+                                <input id="contact" type="text" name="contact"
+                                       class="form-control {{ $errors->has('contact') ? "is-invalid" : "" }}"
+                                       value="{{ $organization->contact }}" placeholder="Contact">
+                                @if($errors->has('contact'))
+                                    <strong class="invalid-feedback">{{ $errors->first('contact') }}</strong>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label for="registration">Registration</label>
+                                <input id="registration" type="text" name="registration"
+                                       class="form-control {{ $errors->has('registration') ? "is-invalid" : "" }}"
+                                       value="{{ $organization->registration }}" placeholder="Registration">
+                                @if($errors->has('registration'))
+                                    <strong class="invalid-feedback">{{ $errors->first('registration') }}</strong>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
